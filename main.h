df987f2428cce63a5703ca15fc5480d0257afda5
Gerrit Pusher <pusher@localhost>
2003-05-10 05:47:50 +0800

    Import bftpd v1.0.24

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef __BFTPD_MAIN_H
+#define __BFTPD_MAIN_H
+
+#include <sys/types.h>
+
+struct bftpd_childpid {
+	pid_t pid;
+	int sock;
+};
+
+extern int global_argc;
+extern char **global_argv;
+extern struct sockaddr_in name;
+extern FILE *passwdfile, *groupfile, *devnull;
+extern char *remotehostname;
+extern struct sockaddr_in remotename;
+extern int control_timeout, data_timeout;
+extern int alarm_type;
+
+/* Command line options */
+char *configpath;
+int daemonmode;
+
+void print_file(int number, char *filename);
+
+#endif
